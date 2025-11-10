8fb3fad279854a0f5b140fd47259704227c10e8b
OxBryte <silascyrax@gmail.com>
2025-11-10 11:53:28 +0100

    made update to  - rc/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { AppKitProvider } from "@reown/appkit";
+import { AppKitProvider } from "@reown/appkit-react";
+import { base, baseSepolia } from "viem/chains";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AppKitProvider projectId={import.meta.env.VITE_PROJECT_ID} network>
+    <AppKitProvider
+      projectId={import.meta.env.VITE_PROJECT_ID}
+      network={[base, baseSepolia]}
+    >
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools />
         <App />
