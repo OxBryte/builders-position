974b7a548c9d5cf2988b75c75aeefe83714f4c82
OxBryte <silascyrax@gmail.com>
2025-11-10 18:19:31 +0100

    made update to  - rc/components/layout/Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -8,7 +8,7 @@ type LayoutProps = {
 function Layout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen">
-      <div className="mx-auto w-full max-w-[720px] px-6">
+      <div className="mx-auto w-full max-w-[860px] px-6">
         <Navbar />
         {children}
       </div>
