475eef61a550f5b0e7636b36c1c0db93c033f8d0
OxBryte <silascyrax@gmail.com>
2025-11-10 12:04:58 +0100

    made update to  - rc/components/layout/Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,19 +1,26 @@
-import { useAppKit } from "@reown/appkit/react";
+import { useAppKit, useAppKitAccount } from "@reown/appkit/react";
+import { truncateAddress } from "../lib/utils";
 
 export default function Navbar() {
   const { open } = useAppKit();
-  const { address } = useApp();
+  const { address, isConnected } = useAppKitAccount();
 
   return (
     <nav className="flex items-center justify-between py-4">
       <span className="text-xl font-semibold tracking-wide">Your Logo</span>
-      <button
-        className="rounded-full border border-blue-500 bg-blue-600 px-4 py-2 font-semibold text-sm text-white transition hover:bg-blue-500"
-        type="button"
-        onClick={() => open()}
-      >
-        Connect Wallet
-      </button>
+      {isConnected ? (
+        <div className="flex items-center gap-2 ">
+          <span>{truncateAddress(address)}</span>
+        </div>
+      ) : (
+        <button
+          className="rounded-full border border-blue-500 bg-blue-600 px-4 py-2 font-semibold text-sm text-white transition hover:bg-blue-500"
+          type="button"
+          onClick={() => open()}
+        >
+          Connect Wallet
+        </button>
+      )}
     </nav>
   );
 }
