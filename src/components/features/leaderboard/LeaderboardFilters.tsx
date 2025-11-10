0eee4a2c5a97dbdd564c1865f021c652bd264ca2
OxBryte <silascyrax@gmail.com>
2025-11-10 20:10:03 +0100

    made update to  - rc/components/features/leaderboard/LeaderboardFilters.tsx

--- src/components/features/leaderboard/LeaderboardFilters.tsx
+++ src/components/features/leaderboard/LeaderboardFilters.tsx
@@ -31,7 +31,7 @@ export default function LeaderboardFilters({
 
   return (
     <div className="space-y-4 mt-3">
-      <div className="flex flex-col  gap-3 border border-gray-200 bg-white p-4 sm:flex-row sm:items-center sm:justify-between">
+      <div className="flex flex-col rounded-lg gap-3 border border-gray-200 bg-white p-4 sm:flex-row sm:items-center sm:justify-between">
         <div className="flex items-center gap-2">
           <label
             htmlFor="sponsor-filter"
