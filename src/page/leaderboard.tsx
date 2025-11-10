4c33fefde5b8b882542f4b19ec7cb545e3c7cbb1
OxBryte <silascyrax@gmail.com>
2025-11-10 19:54:33 +0100

    made update to  - rc/page/leaderboard.tsx

--- src/page/leaderboard.tsx
+++ src/page/leaderboard.tsx
@@ -59,7 +59,7 @@ export default function LeaderboardPage() {
   }, [data?.users, searchTerm]);
 
   return (
-    <section className="mt-8 space-y-10">
+    <section className=" space-y-10">
       <header className="space-y-2">
         <h1 className="text-3xl font-semibold text-gray-900">
           WalletConnect Builders
