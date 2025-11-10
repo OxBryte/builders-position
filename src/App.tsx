c566eeead1589e3e6e578c5c5063b290553ce1e9
OxBryte <silascyrax@gmail.com>
2025-11-10 11:47:58 +0100

    made update to  - rc/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Layout from './components/layout/Layout'
 
 function App() {
 
   return (
-    <Layout>
-      
-    </Layout>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout><h1>Hello World</h1></Layout>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
