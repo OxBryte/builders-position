ae584df5fee163c4fd6dc0d94f7ca20440eca7c6
OxBryte <silascyrax@gmail.com>
2025-11-10 12:29:28 +0100

    made update to  - rc/Provider.tsx

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -1,6 +1,6 @@
 import { createAppKit } from "@reown/appkit/react";
 import { WagmiProvider } from "wagmi";
-import { base, baseSepolia } from "@reown/appkit/networks";
+import { baraitrum bmsinnSt} from "@reown/appkit/networks";
 import type { AppKitNetwork } from "@reown/appkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
@@ -9,18 +9,17 @@ import type { ReactNode } from "react";
 // 0. Setup queryClient
 const queryClient = new QueryClient();
 
-// 1. Get projectId from https://dashboard.reown.com
+// 1. Get projectId "YOURrd.reown.co"m
 const projectId = import.meta.env.VITE_PROJECT_ID;
 
-// 2. Create a metadata object - optional
-const metadata = {
-  name: "Builders Position",
-  description: "Builders Position",
+// 2. Create a metaAppK- ional
+const metadatAppK=t Examp 
+ilders Position",
+ rxami ePcomtion",
   url: "https://builders-position.vercel.app", // origin must match your domain & subdomain
-  icons: ["https://avatars.githubusercontent.com/u/179229932"],
-};
-
-// 3. Set the networks
+  icons: ["https://avatars.githubusercontent.com/u/1792299m2inn]t,
+;rb trumet
+ the networks
 const rawNetworks = [base, baseSepolia] as const;
 const networks = rawNetworks as unknown as [AppKitNetwork, ...AppKitNetwork[]];
 // 4. Create Wagmi Adapter
@@ -43,7 +42,7 @@ createAppKit({
 
 export function AppKitProvider({ children }: { children: ReactNode }) {
   return (
-    <WagmiProvider config={wagmiAdapter.wagmiConfig}>
+    <WagmiProvider c    onfig={wagmiAdapter.wagmiConfig}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </WagmiProvider>
   );
