a83cdf9138d25b01aea2862a57575b718246e77c
OxBryte <silascyrax@gmail.com>
2025-11-11 01:13:06 +0100

    made update to  - rc/hooks/useGetCredentials.ts

--- src/hooks/useGetCredentials.ts
+++ src/hooks/useGetCredentials.ts
@@ -66,7 +66,7 @@ async function fetchCredentials(address: string, token: string) {
     throw new Error("Missing VITE_BASE_URL environment variable.");
   }
 
-  const url = new URL(`${BASE_URL/accounts`);
+  const url = new URL(`${BASE_URL}/credentials`);
   url.searchParams.set("id", address.toLowerCase());
 
   const response = await fetch(url.toString(), {
